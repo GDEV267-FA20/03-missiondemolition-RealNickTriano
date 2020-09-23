9c5ef96b390cff7281f42709a077605e48cf961d
RealNickTriano <70707042+RealNickTriano@users.noreply.github.com>
2020-09-22 22:20:40 -0400

    Added goal and camera reset

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
 public class FollowCam : MonoBehaviour
@@ -32,16 +33,46 @@ public class FollowCam : MonoBehaviour
 
     void FixedUpdate()
     {
+        Vector3 destination;
 
-        // if there's only one line following an if, it doesn't need braces
+        // If there is no poi, return to P:[ 0, 0, 0 ]
 
-        if (POI == null) return; // return if there is no poi                  
+        if (POI == null)
+        {
 
+            destination = Vector3.zero;
 
+        }
+        else
+        {
 
-        // Get the position of the poi
+            // Get the position of the poi
 
-        Vector3 destination = POI.transform.position;
+            destination = POI.transform.position;
+
+            // If poi is a Projectile, check to see if it's at rest
+
+            if (POI.tag == "Projectile")
+            {
+
+                // if it is sleeping (that is, not moving)
+
+                if (POI.GetComponent<Rigidbody>().IsSleeping())
+                {
+
+                    // return to default view
+
+                    POI = null;
+
+                    // in the next update
+
+                    return;
+
+                }
+
+            }
+
+        }
 
         // Limit the X & Y to minimum values
         destination.x = Mathf.Max(minXY.x, destination.x);
